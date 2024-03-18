c88bff1721d2f409048259a7625108af2570f1c7
辉哥 <995631664@qq.com>
2024-03-18 13:30:11 +0800

    Chapter 3 ex01

---
+++ 01_Ch03Ex01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Ch03Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInteger;
+            string myString;
+            myInteger = 25;
+            myString = "\"myInteger\" is";
+            Console.WriteLine($"{myString} {myInteger}");
+        }
+    }
+}
