f9665cfc43bb6ab5db07b45d7858cc8550cf487e
辉哥 <995631664@qq.com>
2024-03-20 20:54:47 +0800

    exercise for Ch03

---
+++ 03_Ch03Exer01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Ch03Exer01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;
+            Console.WriteLine("Please input first number:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please input second number:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please input third number:");
+            c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please input fourth number:");
+            d = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"The product of {a}, {b}, {c} and {d} is :" + $"{a * b * c * d}.");
+        }
+    }
+}
