f6ce5f21a29b39177f3f0872897bc67aa99c5f42
辉辉 <995631664@qq.com>
2024-04-16 23:11:25 +0800

    Chapter 07 exercises

---
+++ 34_Ch07Exer01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _34_Ch07Exer01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                Console.WriteLine($"Loop cycle{i}");
+                if (i == 5000)
+                {
+                    Console.WriteLine(args[999]);
+                }
+            }
+        }
+    }
+}
---
+++ 35_Ch07Exer02/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _35_Ch07Exer02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Orientation myDirection = (Orientation)1;
+            for (byte myByte = 2; myByte < 10; myByte++)
+            {
+                try
+                {
+                    myDirection = checked((Orientation)myByte);
+                    if ((myDirection < Orientation.North) || (myDirection > Orientation.West))
+                    {
+                        throw new ArgumentOutOfRangeException("mybyte", myByte, "Value must be between 1 and 4.");
+                    }
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    Console.WriteLine("xx " + e.Message + " yy");
+                    Console.WriteLine("Assigning default value, Orientation.North");
+                    //myDirection = Orientation.North;
+                }
+                Console.WriteLine($"myDirection = {myDirection}");
+            }
+        }
+    }
+
+    enum Orientation : byte
+    {
+        North = 1,
+        South = 2,
+        East = 3,
+        West = 4
+    }
+}
