41aabf5e90a91fdba58aa6485a07b7409e2c6150
辉辉 <995631664@qq.com>
2024-04-18 10:12:55 +0800

    Chapter 8

---
+++ 36_Ch08Ex01/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _36_Ch08Ex01
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
