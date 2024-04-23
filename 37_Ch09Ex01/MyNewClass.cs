1472551cfb58bccfde88c780869ee6ab8d537fa3
辉辉 <995631664@qq.com>
2024-04-23 12:04:11 +0800

    Chapter 09

---
+++ 37_Ch09Ex01/MyNewClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _37_Ch09Ex01
+{
+    internal class MyNewClass
+    {
+        MyComplexClass myObj2 = new MyComplexClass();
+    }
+}
---
+++ 37_Ch09Ex01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _37_Ch09Ex01
+{
+    public abstract class MyBase { }
+    internal class MyClass : MyBase { }
+    public interface IMyBaseInterface { }
+    internal interface IMyBaseInterface2 { }
+    internal interface IMyInterface : IMyBaseInterface, IMyBaseInterface2 { }
+    internal sealed class MyComplexClass : MyClass, IMyInterface { }
+
+    /// <summary>
+    /// This class contains my program!
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyComplexClass myObj = new MyComplexClass();
+            Console.WriteLine(myObj.ToString());
+        }
+    }
+}
