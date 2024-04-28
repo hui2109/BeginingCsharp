12b0b42113bff8bd4dbb6d5f18784d7b1b0f2efa
辉哥 <995631664@qq.com>
2024-04-28 10:43:11 +0800

    Chapter 10

---
+++ 41_Ch10Ex01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _41_Ch10Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating object myObj...");
+            MyClass myObj = new MyClass("My Object");
+            Console.WriteLine("myObj created.");
+            for (int i = -1; i <= 0; i++)
+            {
+                try
+                {
+                    Console.WriteLine($"\nAttempting to assign {i} to myObj.Val...");
+                    myObj.Val = i;
+                    Console.WriteLine($"Value {myObj.Val} assigned to myObj.Val.");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Exception {e.GetType().FullName} thrown.");
+                    Console.WriteLine($"Message: \n\"{e.Message}\"");
+                }
+            }
+
+            Console.WriteLine("\nOutputting myObj.ToString()...");
+            Console.WriteLine(myObj.ToString());
+            Console.WriteLine("myObj.ToString() Output.");
+            Console.WriteLine("\nmyDoubledIntPro = 5...");
+            Console.WriteLine($"Getting myDoubledIntPro of 5 is {myObj.myDoubledIntProp}");
+        }
+    }
+}
