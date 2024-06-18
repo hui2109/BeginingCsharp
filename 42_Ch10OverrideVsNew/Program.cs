eba88f4e560fd3900f2204bb1a95c80d6fb64f06
辉哥 <995631664@qq.com>
2024-06-18 21:56:50 +0800

    Chapter 10

---
+++ 42_Ch10OverrideVsNew/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _42_Ch10OverrideVsNew
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyDerivedClass myObj = new MyDerivedClass();
+            MyBaseClass myBaseObj;
+            myBaseObj = myObj;
+            myBaseObj.DoSomething();
+        }
+
+        private int someData;
+        public int SomeData => this.someData;
+
+        public class MyBaseClass
+        {
+            public virtual void DoSomething() => Console.WriteLine("Base imp");
+        }
+
+        public class MyDerivedClass : MyBaseClass
+        {
+            public override void DoSomething() => Console.WriteLine("Derived imp");
+            //new public void DoSomething() => Console.WriteLine("Derived imp");
+        }
+    }
---
+++ 43_Ch10Ex02/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _43_Ch10Ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassA myObject = new ClassA();
+            Console.WriteLine($"myObject.State = {myObject.State}");
+
+            ClassA.ClassB myOtherObject = new ClassA.ClassB();
+            myOtherObject.SetPrivateState(myObject, 999);
+            Console.WriteLine($"myObject.State = {myObject.State}");
+        }
+    }
+
+    public class ClassA
+    {
+        private int state = -1;
+        public int State => state;  // "State" property  is read only.
+        public class ClassB
+        {
+            public void SetPrivateState(ClassA target, int newState)
+            {
+                target.state = newState;
+            }
+        }
+
+        public interface IXXX
+        {
+            int MyProperty { get; set; }
+        }
+    }
+}
